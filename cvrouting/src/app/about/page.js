36574b706920d998390b96733f7618205b943b21
Ezequiel Contreras <ezeq317@gmail.com>
2025-01-13 17:44:23 -0700

    fixed the phone sizing and added content to the about page

--- cvrouting/src/app/about/page.js
+++ cvrouting/src/app/about/page.js
@@ -1,10 +1,19 @@
+"use client";
+
+import React from "react";
+import styles from "./About.module.css";
+
 const About = () => {
-    return (
-      <div>
-        <h1>About Us</h1>
-        <p>We are a leading roofing company with over 20 years of experience.</p>
+  return (
+    <section className={styles.aboutSection}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>About Us</h1>
+        <p className={styles.text}>
+          We are a local family-owned and operated roofing company. With 20+ years of experience in the industry, we strive to deliver high-quality and well-built roofs. Our focus points are honest work and strong attention to detail in order to ensure premium roofs. We value all project sizes, from small fixtures to large reroofs, and promise to always exceed our customers’ expectations.
+        </p>
       </div>
-    );
-  }
-  
-  export default About;
+    </section>
+  );
+};
+
+export default About;
--- cvrouting/src/app/components/Header/Header.js
+++ cvrouting/src/app/components/Header/Header.js
@@ -15,7 +15,6 @@ const Header = () => {
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
-          <li><Link href="/services">Services</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
